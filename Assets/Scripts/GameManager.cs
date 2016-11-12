671a0b908816bd10dd8bdfe00d3589fb8dec5b39
David Piper <dbpiper@gmail.com>
2016-11-12 12:18:28 -0600

    Add some basic, but not currently usable, scripts as a first step towards adding game logic.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+
+	}
+
+	void InitGame()
+	{
+		boardScript.SetupScene (level);
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+}
